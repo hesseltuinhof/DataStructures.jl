0af19e75eb3c5c92e7cab057d2bfd25316095406
hesseltuinhof <htuinhof@gmail.com>
2019-01-23 23:50:51 +0100

    export additional functions

--- src/DataStructures.jl
+++ src/DataStructures.jl
@@ -8,12 +8,17 @@ export AbstractList
 export ArrayBasedList, LinkedList, LinkNode
 
 export AbstractStack
+export LinkedStack
 
+export movetostart!, movetoposition!, movetoend!, prev!, next!, currentposition, getvalue
+export topvalue
+
+# TODO put linknode out into own file and put it under abstract includes
 include("./list/abstract_list.jl")
 include("./list/abstract_stack.jl")
 include("./list/array_based_list.jl")
-include("./list/array_based_stack.jl")
+#include("./list/array_based_stack.jl")
 include("./list/linked_list.jl")
-
+include("./list/linked_stack.jl")
 
 end
