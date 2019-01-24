01c57743d1a39b2ff93fa1fdd496b9a5f0dd0eeb
hesseltuinhof <htuinhof@gmail.com>
2019-01-24 22:57:51 +0100

    add linked queue implementation

---
+++ src/list/linked_queue.jl
@@ -0,0 +1,29 @@
+mutable struct LinkedQueue{T} <: AbstractQueue{T}
+    front::LinkNode{T}
+    rear::LinkNode{T}
+    size::Int
+    function LinkedQueue{T}() where T
+        front = rear = LinkNode{T}()
+        size = 0
+        new(front, rear, size)
+    end
+end
+
+
+isempty(q::LinkedQueue) = q.size == 0
+length(q::LinkedQueue) = q.size
+frontvalue(q::LinkedQueue) = isempty(q) ? throw(ArgumentError("Queue is empty")) : q.front.next.data
+
+
+function enqueue!(q::LinkedQueue, item)
+    q.rear = q.rear.next = LinkNode{eltype(q)}(item)
+    q.size += 1
+end
+
+function dequeue!(q::LinkedQueue)
+    (q.size == 0) && throw(ArgumentError("Queue is empty"))
+    item = q.front.next.data
+    q.front.next = q.front.next.next
+    q.size -= 1
+    return item
+end
