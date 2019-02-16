f29ac829b8c9a718d82fcfa1136e5c7616cf1d59
hesseltuinhof <htuinhof@gmail.com>
2019-02-16 22:08:55 +0100

    add complete binary tree and heap implementation

--- src/tree/completebinary_tree.jl
+++ src/tree/completebinary_tree.jl
@@ -1,9 +1,9 @@
 mutable struct CompleteBinaryTree{T} <: AbstractTree{T}
-    tree::Vector{T}
+    data::Vector{T}
     size::Int
     function CompleteBinaryTree{T}(size::Int) where T
-        tree = Vector{T}(undef, size)
-        new(tree, size)
+        data = Vector{T}(undef, size)
+        new(data, size)
     end
     function CompleteBinaryTree{T}(data) where T
         new(data, length(data))
@@ -11,6 +11,8 @@ mutable struct CompleteBinaryTree{T} <: AbstractTree{T}
 end
 
 CompleteBinaryTree(data::Vector) = CompleteBinaryTree{eltype(data)}(data)
+
+# TODO add function such that for t(pos) = t.data[pos] 
 
 # Note: It is assumed that pos ∈ [1,tree.size]. Furthermore, if the result
 # is out of bounds, zero is returned.
@@ -20,4 +22,8 @@ leftchild(t::CompleteBinaryTree, pos) = 2*pos <= t.size ? 2*pos : 0
 rightchild(t::CompleteBinaryTree, pos) = 2*pos + 1 <= t.size ? 2*pos + 1 : 0
 leftsibling(t::CompleteBinaryTree, pos) = pos&1 == 1 ? pos - 1 : 0 
 rightsibling(t::CompleteBinaryTree, pos) = (pos&1 == 0) && (pos + 1 <= t.size) ? pos + 1 : 0
-isleaf(t::CompleteBinaryTree, pos) = (pos > n ÷ 2) && (pos <= t.size)
+isleaf(t::CompleteBinaryTree, pos) = (pos > t.size ÷ 2) && (pos <= t.size)
+
+# TODO this breaks interface of array-based list, where append! is used
+append!(t::CompleteBinaryTree, item) = (t.size += 1; push!(t.data, item))
+swap!(t::CompleteBinaryTree, i, j) = swap!(t.data, i, j)
--- src/tree/heap.jl
+++ src/tree/heap.jl
@@ -1,29 +1,105 @@
-mutable struct Heap{T} <: AbstractTree{T}
+# TODO maxsize should be initialized
+
+#abstract type Heap{T} <: AbstractTree{T} end
+
+mutable struct Heap{T} #<: Heap{T}
     tree::CompleteBinaryTree{T}
     size::Int
     maxsize::Int
-    function Heap{T}(tree) where T
-       tree = tree
-       size = tree.size
-       maxsize = DEFAULT_MAX_SIZE
-       n = new(tree, size, maxsize)
-       heapify!(n)
+    comparator::Function
+    function Heap{T}(data, comparator) where T
+        tree = CompleteBinaryTree{T}(data)
+        size = tree.size
+        maxsize = DEFAULT_MAX_SIZE
+        n = new(tree, size, maxsize, comparator)
+        buildheap!(n)
     end
 end
 
+Heap(data::Vector, comparator) = Heap{eltype(data)}(data, comparator)
 
-function _siftdown! end
+### TODO alias MaxHeap and MinHeap to _Heap, than based on type specify isless function
+# eg. if isa(t, MaxHeap) = (isgreater(x, y) = isless(y,x))
 
-function _siftup! end
+#const MaxHeap{T} = _Heap{T}
+#const MinHeap{T} = _Heap{T}
 
-function push! end
+MinHeap(data::Vector) = Heap(data, isless)
+MaxHeap(data::Vector) = Heap(data, (x,y)->isless(y,x))
 
-function pop! end
 
-function remove! end
 
-function peek end
+function _siftdown!(h::Heap, pos)
+    let i = pos
+        while !isleaf(h.tree, i)
+            j = leftchild(h.tree, i)
+            rc = rightchild(h.tree, i)
+            # check if rightchild is within bounds
+            # (if rightchild is outside, rc will be 0)
+            if (rc != 0) && h.comparator(h.tree.data[rc], h.tree.data[j])
+                j = rc
+            end
+            h.comparator(h.tree.data[i], h.tree.data[j]) && break
+            swap!(h.tree, i, j)
+            i = j
+        end
+    end
+end
 
-function heapify!() end
+function _siftup!(h::Heap, pos)
+    let i = pos
+        while (i != 1) && h.comparator(h.tree.data[i], h.tree.data[parent(h.tree, i)])
+            swap!(h.tree, i, parent(h.tree, i))
+            i = parent(h.tree, i)
+        end
+    end
+end
+
+function insert!(h::Heap, item)
+    (h.size < h.maxsize) || throw(ArgumentError("Heap capacity exceeded"))
+    append!(h.tree, item)
+    h.size += 1; h.tree.size += 1
+    _siftup!(h, h.size)
+end
+    
+
+function remove!(h::Heap)
+    (h.size > 0) || throw(ArgumentError("Heap is empty."))
+    item = h.tree.data[1]
+    swap!(h.tree, 1, h.size)
+    h.size -= 1; h.tree.size -= 1
+    if h.size != 0
+        _siftdown!(h, 1)
+    end
+    return item
+end
+
+function remove!(h::Heap, pos)
+    (1 <= pos <= h.size) || throw(BoundsError("Position out of range"))
+    item = h.tree.data[pos]
+    if pos == h.size
+        h.size -= 1; h.tree.size -= 1
+    else
+        swap!(h.tree, pos, h.size)
+        h.size -= 1; h.tree.size -= 1
+        _siftup!(h, pos)
+        if h.size != 0
+            _siftdown!(h, pos)
+        end
+    end
+    return item
+end
+
+function buildheap!(h::Heap)
+    let i = h.size ÷ 2
+        while i>0
+            _siftdown!(h, i)
+            i -= 1
+        end
+    end
+    return h
+end
+
+peek(h::Heap) = h.tree.data[1]
 # length() = nothing
 # isempty() = nothing
